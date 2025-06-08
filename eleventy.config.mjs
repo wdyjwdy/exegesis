1bc6bcfe5004a81b1c66d8021314874239b76dee
wdyjwdy <xxccat@gmail.com>
2025-06-08 23:47:44 +0800

    init

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,3 +1,10 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
+  eleventyConfig.addPlugin(HtmlBasePlugin);
 }
+
+export const config = {
+  pathPrefix: "/exegesis/",
+};
