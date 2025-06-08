bb618c7d0b1d9847cf7e6e96c903f341415e4813
wdyjwdy <xxccat@gmail.com>
2025-06-09 00:51:13 +0800

    update

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -2,6 +2,7 @@ import { HtmlBasePlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
+  eleventyConfig.addPassthroughCopy("icon.svg");
   eleventyConfig.addPlugin(HtmlBasePlugin);
 }
 
