aeb1a050877ddf0d68d1786a272453aadf06191f
wdyjwdy <xxccat@gmail.com>
2025-06-09 02:59:28 +0800

    update

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,9 +1,15 @@
 import { HtmlBasePlugin } from "@11ty/eleventy";
+import markdownIt from "markdown-it";
+import MarkdownItGitHubAlerts from "markdown-it-github-alerts";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("icon.svg");
   eleventyConfig.addPlugin(HtmlBasePlugin);
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt({ breaks: true }).use(MarkdownItGitHubAlerts),
+  );
 }
 
 export const config = {
